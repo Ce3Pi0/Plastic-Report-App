c35ae2e53ad8b25bc2cfe8a43ade59f2c6c4a0d2
Ce3Pi0 <hristijanbeast2004@gmail.com>
2026-05-04 16:56:11 -0400

    Removed Unused Http Codes

--- Backend/main/config/errorCodes.py
+++ Backend/main/config/errorCodes.py
@@ -1,8 +1,6 @@
 from enum import Enum
 
 class HttpError(Enum):
-    OK = ("OK", 200)
-    CREATED = ("Created", 201)
     BAD_REQUEST = ("Bad request", 400)
     UNAUTHORIZED = ("Unauthorized", 401)
     NOT_FOUND = ("Not found", 404)
